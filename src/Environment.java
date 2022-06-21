7c4d1ef08986847429fb9b8570dc992977ce8dde
aleksandr <apastukhanov@mail.ru>
2022-06-22 00:31:16 +0300

    added config and Enviroment class

---
+++ src/Environment.java
@@ -0,0 +1,31 @@
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Environment {
+    private Properties prop;
+    final String propFileName = "config.properties";
+
+    Environment() {
+        initProperties();
+    }
+
+    private void initProperties() {
+        this.prop = new Properties();
+
+        InputStream inputStream = getClass().getClassLoader().
+                getResourceAsStream(this.propFileName);
+
+        if (inputStream!=null) {
+            try {
+            prop.load(inputStream);
+            } catch (Exception ex) {
+                System.out.println(ex.getStackTrace());
+            }
+        }
+    }
+
+    public String getEnv(String key) {
+        return this.prop.getProperty(key);
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,14 @@ public class Main {
         Coordinates coord = new Coordinates();
         coord.getGPSCoordinates();
 
+        Environment env = new Environment();
+
+        System.out.println(env.getEnv("OPENWEATHER_API"));
+        System.out.println(env.getEnv("OPENWEATHER_URL"));
+        System.out.println(env.getEnv("USE_ROUNDED_COORDS"));
+        System.out.println(env.getEnv("USE_"));
+
+
+
     }
 }
